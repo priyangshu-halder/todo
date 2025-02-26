d2e3f9a9ccefbb28876453a02728a333d92d1d54
Priyangshu <contact.priyangshu.work@gmail.com>
2025-02-26 17:07:56 +0530

    commit

--- main/todo/forms.py
+++ main/todo/forms.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Task
+
 class TaskForm(ModelForm):
     class Meta:
         model=Task
@@ -7,4 +10,14 @@ class TaskForm(ModelForm):
             'unique_id',
             'task',
         ]
-    
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=[
+            'username',
+            'email',
+            'password1',
+            'password2',
+        ]
+        
--- main/todo/urls.py
+++ main/todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import index, edit, delete
+from .views import index, edit, delete, registerpage, loginpage
 urlpatterns = [
     path('', index, name="index"),
     path('edit/<int:pk>/', edit, name="edit"),
     path('delete/<int:pk>/', delete, name="delete"),
+    path('signup/', include("django.contrib.auth.urls")),
+    path('signup', registerpage, name="signup"),
+    path('login', loginpage, name="login"),
 ]
