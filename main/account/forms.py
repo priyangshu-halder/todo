c5ac9f2010c3e49a50319d2c480c2f2bc91d70a8
Priyangshu <contact.priyangshu.work@gmail.com>
2025-02-27 20:36:43 +0530

    add account feature

--- main/account/forms.py
+++ main/account/forms.py
@@ -1,15 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
-from django.forms import ModelForm
-from .models import Task
-
-class TaskForm(ModelForm):
-    class Meta:
-        model=Task
-        fields=[
-            'unique_id',
-            'task',
-        ]
 
 class CreateUserForm(UserCreationForm):
     class Meta:
--- main/main/urls.py
+++ main/main/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from todo import urls
+from account import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
+    path('account/', include('account.urls')),
 ]
